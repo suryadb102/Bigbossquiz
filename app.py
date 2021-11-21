9f7a6c05ceccc3ee5353aa2c667312df8958563e
nextbigthing5317 <94757436+nextbigthing5317@users.noreply.github.com>
2021-11-21 19:17:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import jwt
 import pyrebase
 import sklearn
 import pandas as pd
+import numpy
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
