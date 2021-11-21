0924275c308c072461596e5687941eb6aea3c135
nextbigthing5317 <94757436+nextbigthing5317@users.noreply.github.com>
2021-11-21 19:29:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import random
 import jwt
 import pyrebase
 import sklearn
-import pandas as pd
 import numpy
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
